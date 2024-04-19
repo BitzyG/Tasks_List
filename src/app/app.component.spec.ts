b9981da007039bca7da8c2e0f7ea6fa299c57bb2
Itzel Andrea <gv.itzelandrea@gmail.com>
2024-04-18 21:32:08 -0500

    ADD | Spec configurations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Tasks_List');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Tasks_List app is running!');
-  });
 });
